0c3c7710b81252a6b3f21cc6ec13fb5eda78b010
David Hochbaum <dhochbaum+flatiron@gmail.com>
2019-06-05 18:57:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,12 +29,12 @@ function checkCollision(rock) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
 
     // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
-    const dodgerRightEdge = 0;
+    const dodgerRightEdge = dodgerLeftEdge + 40;
 
     const rockLeftEdge = positionToInteger(rock.style.left)
 
     // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
-    const rockRightEdge = 0;
+    const rockRightEdge = rockLeftEdge + 20;
 
     if (false /**
                * Think about it -- what's happening here?
@@ -119,6 +119,16 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
+
+   document.addEventListener('keydown', function(e) {
+     if (e.which === LEFT_ARROW) {
+       moveDodgerLeft()
+     }
+     if (e.which === RIGHT_ARROW) {
+       moveDodgerRight()
+     }
+   })
+
 }
 
 function moveDodgerLeft() {
@@ -127,6 +137,15 @@ function moveDodgerLeft() {
    * This function should move DODGER to the left
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
+
+   window.requestAnimationFrame(function() {
+     const left = positionToInteger(DODGER.style.left)
+
+     if (left > 0) {
+       DODGER.style.left = `${left - 4}px`;
+     }
+   })
+
 }
 
 function moveDodgerRight() {
@@ -135,6 +154,14 @@ function moveDodgerRight() {
    * This function should move DODGER to the right
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
+   window.requestAnimationFrame(function() {
+     const left = positionToInteger(DODGER.style.left)
+
+     if (left < 360) {
+       DODGER.style.left = `${left + 4}px`;
+     }
+   })
+
 }
 
 /**
