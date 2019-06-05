d1d8bca0f66b8d6717fb20575834bd57394d2d6f
David Hochbaum <dhochbaum+flatiron@gmail.com>
2019-06-05 19:18:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -36,8 +36,13 @@ function checkCollision(rock) {
     // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
     const rockRightEdge = rockLeftEdge + 20;
 
+    return (
+      (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) ||
+      (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) ||
+      (rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge)
+    )
 
-    if (false
+//    if (false
 /**
                * Think about it -- what's happening here?
                * There's been a collision if one of three things is true:
@@ -48,8 +53,8 @@ function checkCollision(rock) {
                * 3. The rock's left edge is < the DODGER's right edge,
                *    and the rock's right edge is > the DODGER's right edge
                */) {
-      return true
-    }
+  //    return true
+  //  }
 /*
     if((rockLeftEdge<dodgerLeftEdge)&&(rockRightEdge>dodgerLeftEdge)) {
       return true
